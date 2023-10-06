c8db260764eb3682df051619d8f9edfb3095f361
boy2balls <nguyendanghoaa@gmail.com>
2023-10-06 15:12:47 +0700

    chore: add table ship and related relationship

--- src/vessel/dto/create-vessel.dto.ts
+++ src/vessel/dto/create-vessel.dto.ts
@@ -1,1 +1,9 @@
-export class CreateVesselDto {}
+import { IsNotEmpty } from 'class-validator';
+
+export class CreateVesselDto {
+  @IsNotEmpty()
+  name: string;
+
+  @IsNotEmpty()
+  maxWeight: number;
+}
--- src/vessel/vessel.controller.ts
+++ src/vessel/vessel.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpException,
+  HttpStatus,
+  Post,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
 import { VesselService } from './vessel.service';
 import { CreateVesselDto } from './dto/create-vessel.dto';
 import { AuthGuard } from '@nestjs/passport';
@@ -9,7 +17,11 @@ export class VesselController {
   constructor(private readonly vesselService: VesselService) {}
 
   @Post('create-ship')
-  create(@Body() createVesselDto: CreateVesselDto) {
-    return this.vesselService.createShip(createVesselDto);
+  create(@Body(ValidationPipe) createVesselDto: CreateVesselDto) {
+    try {
+      return this.vesselService.createShip(createVesselDto);
+    } catch (err) {
+      throw new HttpException('Error', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
